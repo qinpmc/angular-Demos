7520422a16986eacb7d1fb41343d77c4bb769940
qinpmc <850823973@qq.com>
2018-11-10 22:00:38 +0800

    add input demo

---
+++ ch3组件通讯/commu/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OrderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
