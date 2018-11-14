5793d9da53f4f62470eac6c220a35612c1ba0b28
qinpmc <850823973@qq.com>
2018-11-14 22:33:21 +0800

    add docheck

--- ch3组件通讯/lifeCycle1/src/app/child/child.component.ts
+++ ch3组件通讯/lifeCycle1/src/app/child/child.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit,Input,OnChanges,SimpleChanges} from '@angular/core';
+import { Component, OnInit,Input,OnChanges,SimpleChanges,DoCheck} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit,OnChanges {
+export class ChildComponent implements OnInit,OnChanges,DoCheck {
+
 
   @Input()
   greeting:string;
@@ -14,7 +15,9 @@ export class ChildComponent implements OnInit,OnChanges {
   user:{name:string}
 
   message:string ="child 初始message";
-
+  oldUsername:string;
+  isChanged:boolean;
+  noChangeCount:number;
   constructor() { }
 
   ngOnInit() {
@@ -22,6 +25,20 @@ export class ChildComponent implements OnInit,OnChanges {
 
   ngOnChanges(changes:SimpleChanges):void {
     console.log(JSON.stringify(changes, null, 4));
+  }
+  ngDoCheck():void {
+    if(this.user.name !==this.oldUsername){
+      this.isChanged = true;
+      console.log("ngDoCheck: user.name 从"+this.oldUsername+"变成了"+this.user.name);
+      this.oldUsername = this.user.name;
+    }
+    if(this.isChanged){
+      this.noChangeCount =0;
+    }else{
+      this.noChangeCount +=1;
+      console.log("ngDoCheck: user.name没变化，但ngDoCheck 已被调用"+this.noChangeCount+"次数");
+    }
+    this.isChanged = false;
   }
 
 }
