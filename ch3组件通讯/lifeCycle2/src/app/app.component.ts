d3e72400d8cfc9030cd539981b8dbd95e20a3d0a
qinpmc <850823973@qq.com>
2018-11-16 21:13:32 +0800

    add viewcheck2

--- ch3组件通讯/lifeCycle2/src/app/app.component.ts
+++ ch3组件通讯/lifeCycle2/src/app/app.component.ts
@@ -8,11 +8,7 @@ import {Child2Component} from "./child2/child2.component";
 })
 export class AppComponent implements OnInit,AfterViewInit,AfterViewChecked  {
 
-  title = 'lifeCycle1';
-
   greeting:string = "父组件的greeting";
-
-  user:{name:string} = {name:"父组件的userName=Tom"};
 
   @ViewChild("child2_1")
   child2_1:Child2Component;
@@ -20,14 +16,18 @@ export class AppComponent implements OnInit,AfterViewInit,AfterViewChecked  {
   ngOnInit():void{
     setInterval(() =>{
       this.child2_1.greeting("父组件调用子组件child2的greeting方法");
-    })
+    },3000)
 
   }
   ngAfterViewInit():void {
-    console.log("父组件ngAfterViewInit");
+    console.log("父组件ngAfterViewInit"); //执行一次
+    setTimeout(() =>{                       //修改属性，必须写在下一个周期中
+      this.greeting = "新的父组件greeting"
+    },0)
   }
 
   ngAfterViewChecked():void {
-    console.log("父组件ngAfterViewChecked");
+    console.log("父组件ngAfterViewChecked"); //上文定时器执行一次，此方法执行一次
   }
+
 }
--- ch3组件通讯/lifeCycle2/src/app/child2/child2.component.ts
+++ ch3组件通讯/lifeCycle2/src/app/child2/child2.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit,AfterViewInit,AfterViewChecked  } from '@angular/core
 export class Child2Component implements OnInit,AfterViewInit,AfterViewChecked {
 
   ngAfterViewInit():void {
-    console.log("子组件ngAfterViewInit");
+    console.log("子组件ngAfterViewInit"); //此方法执行一次，
   }
 
   ngAfterViewChecked():void {
-    console.log("子组件ngAfterViewChecked");
+    console.log("子组件ngAfterViewChecked"); //父组件定时器执行一次，此方法执行一次，先于父组件执行
   }
 
   constructor() { }
