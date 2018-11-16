8b78bac3f606ef36863c467de41162d3a25c7dd4
qinpmc <850823973@qq.com>
2018-11-16 21:47:47 +0800

    add ng-content

--- ch3组件通讯/lifeCycle2/src/app/app.module.ts
+++ ch3组件通讯/lifeCycle2/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Child2Component } from './child2/child2.component';
+import { Child3Component } from './child3/child3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Child2Component
+    Child2Component,
+    Child3Component
   ],
   imports: [
     BrowserModule,
