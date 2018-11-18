a1d9347e507072d41d78ccb0a7ee759a5525eb3e
qinpmc <850823973@qq.com>
2018-11-18 23:23:50 +0800

    add templateform

---
+++ ch4表单/form1/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'form1';
+
+  onSubmit(value:any){
+    console.log(value);
+  }
+}
---
+++ ch4表单/form1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { HttpModule } from '@angular/http'
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
